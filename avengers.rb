b0b53b2f930bdc55ec29c17ad8ec816d40c0688e
ed-commits <edward.alexander.cant@gmail.com>
2020-05-13 13:49:07 +0100

    added names and attacks

--- avengers.rb
+++ avengers.rb
@@ -1,5 +1,17 @@
 avengers = {
-    iron_man: {},
-    hulk: {},
-    batman: {}
+    iron_man: {
+        name: "Tony Stark",
+        attack: "Punch",
+        power: 10
+    },
+    hulk: {
+        name: "Bruce Banner",
+        attack: "Slam",
+        power: 50
+    },
+    batman: {
+        name: "Batman",
+        attack: "Bat-kick",
+        power: 30
+    }
 }
